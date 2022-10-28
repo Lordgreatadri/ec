d9f12bf505f10ea888a8fb588043e0fc35dcb3db
Emmanuel Lordgreat-Adri <rexmerlo@gmail.com>
2022-10-28 07:51:25 -0700

    Update db_cn.php

--- view/controllers/db_cn.php
+++ view/controllers/db_cn.php
@@ -8,7 +8,7 @@ class DBCon{
      * Database class
      */
     private $db_host = " .0.0.1";  // Change as required  127.0.0.1
-    private $db_user = "root";  // Change as required
+    private $db_user = " ";  // Change as required
     private $db_pass = " (  P^ %F"; // !@b)o // (3P^ %F  '# ~'    Change as required
     private $db_name = "mtncare247";    // Change as required //civil_service
    
